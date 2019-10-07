bf405459b4012d2fb050ed8bec8c702ed71dcea7
Joseph <josephclorenzo@gmail.com>
2019-10-07 10:56:27 -0700

    styling user comments and leftover button

--- bookends-frontend/src/review/UsersBookReview.js
+++ bookends-frontend/src/review/UsersBookReview.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+import randomColorGenerator from "../styles/randomColorGenerator";
 
 class UsersBookReview extends Component {
 
     state = {
-        renderCommentBox: false
+        renderCommentBox: false,
+        backgroundColor: "#000"
     }    
+
+    componentDidMount() {
+        const color = randomColorGenerator();
+        this.setState({backgroundColor: color});
+    }
 
     renderComment = () => {
         this.setState({
@@ -14,22 +22,44 @@ class UsersBookReview extends Component {
 
     render() {
         const divStyles = {
-            width: "50%", 
-            margin: "0 auto", 
-            border: "1px solid black"
+            backgroundColor: this.state.backgroundColor
         }
 
         const { review, user } = this.props
         console.log(this.props)
         return (
-            <div style={divStyles}>
-                <p>{review.title} </p>
-                <p>{review.content} </p> 
-                <p>{user.username}</p>
+            <CommentContainer style={divStyles}>
+                <Title>{review.title} </Title>
+                <Content>{review.content} </Content> 
+                <UserInfo>Posted By: {user.username}</UserInfo>
 
-                </div>
+            </CommentContainer>
         )
     }
 }
 
 export default UsersBookReview;
+
+const CommentContainer = styled.div`    
+    width: 100%; 
+    margin: 0 auto;
+    margin-top: 10px;
+    padding: 10px;
+`
+
+const Title = styled.h2`
+    font-family: Playfair Display, serif; 
+    background-color: white;
+    padding-left: 5px;
+`
+
+const Content = styled.p`
+    font-family: Lato, sans-serif;
+    background-color: white;
+    padding: 0 5px 0 5px;
+`
+
+const UserInfo = styled.p`
+    font-size: 12px;
+    font-family: Lato, sans-serif;
+`
