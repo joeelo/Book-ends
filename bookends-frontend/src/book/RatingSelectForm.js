a17ddab239b5fff80440fe674d5e3800dbacb24d
Joe Lorenzo <josephclorenzo@gmail.com>
2020-07-05 22:34:44 -0700

    updaet

--- bookends-frontend/src/book/RatingSelectForm.js
+++ bookends-frontend/src/book/RatingSelectForm.js
@@ -1,67 +1,68 @@
-import React, { Component } from 'react'
+import React, { useState, useContext } from 'react'
 import styled from "styled-components";
 import { UserContext } from '../context/UserContext';
 
-export default class RatingSelectForm extends Component {
+const RatingSelectForm = (props) => {
 
-    state = {
-        selectValue: 1
-    }
+	const [ selectValue, setSelectValue ] = useState(1);
+	const { user } = useContext(UserContext);
 
-    handleSelectChange = (event) => {
-        this.setState({selectValue: event.target.value});
-    }
 
-    handleSubmit = async (event) => {
-        const user = this.context; 
-        event.preventDefault();
-        try {
-            const data = {
-                user: this.state.user,
-                rating: parseInt(this.state.selectValue),
-                book: this.props.book
-            }
-            const url = "http://localhost:3000/rating"
-            const config = {
-                method: "PUT",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }
-            let response = await fetch(url, config);
-            let json = await response.json();
-            console.log(json);
-        } catch (error) {
-            console.log(error);
-        }
+	const handleSelectChange = (event) => {
+		setSelectValue(event.target.value);
+	}
 
-    }
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const data = {
+				user: user,
+				rating: parseInt(selectValue),
+				book: props.book
+			}
+			const url = "http://localhost:3000/rating"
+			const config = {
+				method: "PUT",
+				mode: "cors",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(data)
+			}
+			let response = await fetch(url, config);
+			let json = await response.json();
+			console.log(json);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <SelectForm value={this.state.selectValue} onChange={this.handleSelectChange}> 
-                            <option value="1"> 1 </option>
-                            <option value="2"> 2 </option>
-                            <option value="3"> 3 </option>
-                            <option value="4"> 4 </option>
-                            <option value="5"> 5 </option>
-                        </SelectForm>
-                        <button> submit </button>
-                    </label>
-
-                </form>
-            </div>
-        )
-    }
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>
+					<SelectForm value={selectValue} onChange={handleSelectChange}> 
+						<option value="1"> 1 </option>
+						<option value="2"> 2 </option>
+						<option value="3"> 3 </option>
+						<option value="4"> 4 </option>
+						<option value="5"> 5 </option>
+					</SelectForm>
+					<button> submit </button>
+				</label>
+			</form>
+		</div>
+	)
 }
 
+export default RatingSelectForm; 
+
+RatingSelectForm.defaultProps = {
+	book: {}
+};
+
 const SelectForm = styled.select`
-    height: 20px;
-    width: 60px;
-    margin-top: 20px;
+	height: 20px;
+	width: 60px;
+	margin-top: 20px;
 `
