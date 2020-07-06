f2c6f2dad4ce95652dd88f679c8b5584d9c4b928
Joe Lorenzo <josephclorenzo@gmail.com>
2020-07-05 22:57:03 -0700

    update

--- bookends-frontend/src/buttons/NoteButton.js
+++ bookends-frontend/src/buttons/NoteButton.js
@@ -1,34 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom"
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'
 
 const NoteButton = (props) => {
-    return (
-        <Link to="/note"> 
-            <NoteBtn>
-                <AddButton> + </AddButton>
-            </NoteBtn>
-            </Link>
-    )
+	return (
+	<Link to="/note"> 
+		<NoteBtn>
+			<AddButton> <FontAwesomeIcon icon={faPencilAlt} style={{width: '25px'}}/> </AddButton>
+		</NoteBtn>
+	</Link>
+	)
 }
 
 export default NoteButton;
 
 const NoteBtn = styled.div`
-    top: 30%;
-    left: 20px;
-    position: fixed;
-    background-color: white;
-    height: 50px;
-    width: 50px;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(250, 150, 150, 0.8);
+	top: 30%;
+	left: 20px;
+	position: fixed;
+	background-color: white;
+	height: 50px;
+	width: 50px;
+	border-radius: 50%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: rgba(250, 150, 150, 0.8);
 `
 
 const AddButton = styled.span`
-    font-size: 3em;
-    color: white;
+	font-size: 3em;
+	color: white;
 `
